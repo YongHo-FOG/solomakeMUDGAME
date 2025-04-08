0f8c9df85655bf0960d1676acc182c0b8f48d00e
YongHo-FOG <dydgh9832@gmail.com>
2025-04-08 09:45:01 +0900

    directing
    진행중

---
+++ MiniGame/MiniGame/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+
+namespace MiniGame
+{
+    internal class Program
+    {
+        public class Player
+        {
+            public string Name;
+            public int HP;
+            public int MaxHP;
+            public int Attack;
+            public List<Item> Inventory = new List<Item>();
+
+            public void AttackMonster(Monster monster)
+            {
+                Console.WriteLine($"{Name}이(가) {monster.Name}을(를) 공격했습니다!");
+                monster.HP -= this.Attack;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
